cba5ee48e1cb05657fc3b43a8dbb86fd4d50514b
David Hendricks <david.hendricks@gmail.com>
2021-11-27 21:20:37 -0800

    Enable building a toolchain for Linux
    Leverage coreboot's buildgcc to build a suitable Linux toolchain if
    needed.
    
    Signed-off-by: David Hendricks <david.hendricks@gmail.com>

--- tools/build_toolchain.sh
+++ tools/build_toolchain.sh
@@ -19,26 +19,41 @@ cleanup
 # This will be used by the wget script to record external dependencies while fetching.
 export EXTERNAL_DEPS_FILE
 
-if [ -z "${ARCH##x86*}" ] || [ -z "${ARCH##amd64}" ]; then
-	ARCH=i386
-elif [ -z "${ARCH##ppc*}" ]; then
-	ARCH=ppc64
-fi
+if [ "$1" == "coreboot" ]; then
+	if [ -z "${ARCH##x86*}" ] || [ -z "${ARCH##amd64}" ]; then
+		ARCH=i386
+	elif [ -z "${ARCH##ppc*}" ]; then
+		ARCH=ppc64
+	fi
 
-if ! MAKEFLAGS="" PATH="${TOOLS_DIR}:${PATH}" "${MAKE}" -C "${COREBOOT_BUILD_DIR}" crossgcc-${ARCH} BUILD_LANGUAGES=c; then
-  {
-    echo
-    echo "=== Toolchain build failed"
-    if [ -f "${EXTERNAL_DEPS_FILE}" ]
-    then
-      echo
-      echo -n "One or more additional external dependencies are required for the build: "
-      cat "${EXTERNAL_DEPS_FILE}"
-      echo
-      echo "Please re-run build with USE_FWDPROXY=1 to collect all the required files."
-    fi
-  } >&2
-  exit 1
+	if ! MAKEFLAGS="" PATH="${TOOLS_DIR}:${PATH}" "${MAKE}" -C "${COREBOOT_BUILD_DIR}" crossgcc-${ARCH} BUILD_LANGUAGES=c; then
+	  {
+	    echo
+	    echo "=== Toolchain build failed"
+	    if [ -f "${EXTERNAL_DEPS_FILE}" ]
+	    then
+	      echo
+	      echo -n "One or more additional external dependencies are required for the build: "
+	      cat "${EXTERNAL_DEPS_FILE}"
+	      echo
+	      echo "Please re-run build with USE_FWDPROXY=1 to collect all the required files."
+	    fi
+	  } >&2
+	  exit 1
+	fi
+elif [ "$1" == "linux" ]; then
+	rc=0
+	pushd .
+	cd "${COREBOOT_BUILD_DIR}/util/crossgcc"
+	if ! ./buildgcc -p ${ARCH}-linux -j$(nproc --ignore=1); then
+	  {
+		echo
+		echo "=== Toolchain build failed"
+		rc=1
+	  } >&2
+	fi
+	popd
+	exit $rc
 fi
 
 if [ -f "${EXTERNAL_DEPS_FILE}" ]
