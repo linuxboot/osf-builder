b78a8fbf728dc25417c510ef86de8a7aab38591f
Johnny Lin <johnny_lin@wiwynn.com>
2022-11-24 11:31:21 +0800

    Add a sample vpd.sh for qemu
    Signed-off-by: Johnny Lin <johnny_lin@wiwynn.com>

---
+++ examples/qemu/vpd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+VPD="../../tools/vpd"
+ROM="build/qemu-x86_64/coreboot/build/coreboot.rom"
+
+#Or don't add '-O' that reformats the RO_VPD created by osf-builder/Makefile.inc
+$VPD -f $ROM -O -i RO_VPD -s fsp_log_enable=0
+$VPD -f $ROM -i RO_VPD -s test_key=123
+$VPD -f $ROM -O -i RW_VPD
+
